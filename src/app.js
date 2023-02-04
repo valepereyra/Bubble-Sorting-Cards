bff3152e99d231880dae9f9b9e7389b33379b28f
Valentina Pereyra Castillo <valepereyracastillo@gmail.com>
2023-02-04 17:46:36 +0000

    bubble sorting pronto

--- src/app.js
+++ src/app.js
@@ -6,67 +6,140 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let arrCard = [];
 
-  // agrego evento a form
-  let form = document.querySelector("form");
-  form.addEventListener("submit", ramdomCard);
-
-  //esta funcion genera UNA carta ramdom
-  function ramdomCard() {
+  function randomCard() {
     const palos = ["♦", "♥", "♠", "♣"];
-    const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
-    // traigo el valor del input y lo guardo en cantidadCartas
-    let input = document.getElementById("input");
-    let cantidadCartas = input.value;
+    let indexPalo = Math.floor(Math.random() * palos.length);
+    let indexNumero = Math.floor(Math.random() * numeros.length);
 
-    // //GENERAMOS LAS POSICIONES RAMDOM
-    // let indexPalo = Math.floor(Math.random() * palos.length);
-    // let indexNumero = Math.floor(Math.random() * numeros.length);
+    if (palos[indexPalo] == "♥" || palos[indexPalo] == "♦") {
+      document.getElementById(
+        "card"
+      ).innerHTML += `<div class="card d-flex p-1 m-3 border border-dark" style="width: 190px; height: 15rem; justify-content: space-between; border-radius: 15px;  font-size: 40px; margin: 5px;">
+          <div class="topIcon text-start text-danger ms-2">${palos[indexPalo]}</div>
+          <div class="number text-center">${numeros[indexNumero]}</div>
+          <div class="bottomIcon text-danger mx-2" style="transform:rotate(180deg);">${palos[indexPalo]}</div>
+          </div>`;
+      arrCard.push({
+        palo: palos[indexPalo],
+        numero: numeros[indexNumero]
+      });
+    } else {
+      document.getElementById(
+        "card"
+      ).innerHTML += `<div class="card d-flex p-1 m-3 border border-dark" style="width: 190px; height: 15rem; justify-content: space-between; border-radius: 15px;  font-size: 40px; margin: 5px;">
+              <div class="topIcon text-start ms-2" >${palos[indexPalo]}</div>
+              <div class="number text-center">${numeros[indexNumero]}</div>
+              <div class="bottomIcon mx-2" style="transform:rotate(180deg);">${palos[indexPalo]}</div
+          </div>`;
+      arrCard.push({
+        palo: palos[indexPalo],
+        numero: numeros[indexNumero]
+      });
+    }
+    return arrCard;
+  }
 
-    // console.log(palos[indexPalo], numeros[indexNumero]);
-    //obteniendo elementos
-    // let top = document.getElementById("top");
-    // let number = document.getElementById("number");
-    // let bottom = document.getElementById("bottom");
+  function sortCard() {
+    document.getElementById("card").innerHTML = "";
+    for (let i = 0; i <= arrCard.length - 1; i++) {
+      let indexPalo = arrCard[i].palo;
+      let indexNumero = arrCard[i].numero;
 
-    //ingresar valores
-    // top.innerHTML = palos[indexPalo];
-    // number.innerHTML = numeros[indexNumero];
-    // bottom.innerHTML = palos[indexPalo];
-    let arr = [];
-    for (let i = 1; i <= cantidadCartas; i++) {
-      //GENERA POR CADA VUELTA UNA NUEVA POSICION RAMDOM DE PALO Y DE NUMERO
-      //GENERAMOS LAS POSICIONES RAMDOM
-      let indexPalo = Math.floor(Math.random() * palos.length);
-      let indexNumero = Math.floor(Math.random() * numeros.length);
-      console.log(palos[indexPalo], numeros[indexNumero]);
-
-      arr.push({ palos: palos[indexPalo], numeros: numeros[indexNumero] });
-      console.log(arr);
-
-      if (palos[indexPalo] === "♥" || palos[indexPalo] === "♦") {
-        document.body.innerHTML += `<div class="card" style="width: 18rem;">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item text-danger" id="top">${palos[indexPalo]}</li>
-          <li class="list-group-item text-danger" id="number">${numeros[indexNumero]}</li>
-          <li class="list-group-item text-danger" id="bottom">${palos[indexPalo]}</li>
-        </ul>
-      </div>`;
+      if (indexPalo == "♥" || indexPalo == "♦") {
+        document.getElementById(
+          "card"
+        ).innerHTML += `<div class="card d-flex p-1 m-3 border border-dark" style="width: 190px; height: 15rem; justify-content: space-between; border-radius: 15px;  font-size: 40px; margin: 5px;">
+          <div class="topIcon text-start text-danger ms-2">${indexPalo}</div>
+          <div class="number text-center">${indexNumero}</div>
+          <div class="bottomIcon text-danger mx-2" style="transform:rotate(180deg);">${indexPalo}</div>
+      </div>
+  </div>`;
       } else {
-        document.body.innerHTML += `<div class="card" style="width: 18rem;">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item" id="top">${palos[indexPalo]}</li>
-          <li class="list-group-item" id="number">${numeros[indexNumero]}</li>
-          <li class="list-group-item" id="bottom">${palos[indexPalo]}</li>
-        </ul>
-      </div>`;
+        document.getElementById(
+          "card"
+        ).innerHTML += `<di class="card d-flex p-1 m-3 border border-dark" style="width: 190px; height: 15rem; justify-content: space-between; border-radius: 15px;  font-size: 40px; margin: 5px;">
+          <div class="topIcon text-start ms-2">${indexPalo}</div>
+          <div class="number text-center">${indexNumero}</div>
+          <div class="bottomIcon mx-2" style="transform:rotate(180deg);>${indexPalo}</div>
+        </div>`;
       }
     }
-    return arr;
+    arrCard = [];
   }
-  ramdomCard();
 
-  let botonOrdenar = document.getElementById("botonOrdenar");
+  let cartas = [];
+
+  function cartasInput(input) {
+    document.getElementById("card").innerHTML = "";
+    for (let i = 1; i <= input; i++) {
+      let newCard = randomCard();
+      cartas.push(newCard);
+    }
+  }
+
+  function letrasANumeros() {
+    for (let i = 0; i <= arrCard.length - 1; i++) {
+      if (arrCard[i].numero === "A") {
+        arrCard[i].numero = 14;
+      }
+      if (arrCard[i].numero === "K") {
+        arrCard[i].numero = 13;
+      }
+      if (arrCard[i].numero === "Q") {
+        arrCard[i].numero = 12;
+      }
+      if (arrCard[i].numero === "J") {
+        arrCard[i].numero = 11;
+      }
+    }
+  }
+
+  function numerosALetras() {
+    for (let i = 0; i <= arrCard.length - 1; i++) {
+      if (arrCard[i].numero === 14) {
+        arrCard[i].numero = "A";
+      }
+      if (arrCard[i].numero === 13) {
+        arrCard[i].numero = "K";
+      }
+      if (arrCard[i].numero === 12) {
+        arrCard[i].numero = "Q";
+      }
+      if (arrCard[i].numero === 11) {
+        arrCard[i].numero = "J";
+      }
+    }
+  }
+
+  const bubbleSort = arr => {
+    let wall = arr.length - 1;
+    while (wall > 0) {
+      let index = 0;
+      while (index < wall) {
+        if (arr[index].numero > arr[index + 1].numero) {
+          let aux = arr[index];
+          arr[index] = arr[index + 1];
+          arr[index + 1] = aux;
+        }
+        index++;
+      }
+      wall--;
+    }
+    return arr;
+  };
+
+  document.getElementById("barajarcartas").addEventListener("click", () => {
+    let valor = document.getElementById("input");
+    cartasInput(valor.value);
+  });
+  document.getElementById("ordenarcartas").addEventListener("click", () => {
+    letrasANumeros();
+    bubbleSort(arrCard);
+    numerosALetras();
+    sortCard();
+  });
 };
